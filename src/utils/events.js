66e996c553b4f90e0515f00b2759dde376ff69e9
hekotok <dan_zhel@mail.ru>
2023-11-28 14:53:10 +0200

    reminders

--- src/utils/events.js
+++ src/utils/events.js
@@ -4,15 +4,15 @@ import { events } from './admin.js'
 
 export const activeEvents = {}
 
-export const chooseEvent = async chatId => {
+export const chooseEvent = async chatId => new Promise(() => {
 	if (events.length)
-		await bot.sendMessage(
+		bot.sendMessage(
 			chatId,
 			'Выберите мероприятие',
 			{ reply_markup: { inline_keyboard: splitArray(events, 3) } }
 		)
 	else {
-		await bot.sendMessage(chatId, 'Сейчас не запланировано никаких мероприятий')
+		bot.sendMessage(chatId, 'Сейчас не запланировано никаких мероприятий')
 
 		return
 	}
@@ -22,8 +22,33 @@ export const chooseEvent = async chatId => {
 		activeEvents[chatId].push(events.find(event => event.text === data))
 
 		await bot.sendMessage(chatId, `Вы подписались на мероприятие ${data}`)
+		activeEvents[chatId].forEach(event => addReminder(chatId, event))
+
 		bot.off('callback_query', handleCallbackQuery)
 	}
 
 	bot.on('callback_query', handleCallbackQuery)
+})
+
+export const addReminder = (chatId, event) => {
+	const { message, date } = event
+
+	const [ day, month, year ] = date.split`.`
+	const reminderTimes = [ '6:00', '10:00', '11:00' ]
+
+	console.log('add')
+
+	setInterval(() => {
+		const currentDate = new Date()
+
+		reminderTimes.forEach(time => {
+			const [ hours, minutes ] = time.split(':')
+			const reminderDate = new Date(year, month - 1, day, hours, minutes)
+
+			console.log(time, currentDate)
+
+			if (currentDate.getTime() >= reminderDate.getTime() && currentDate.getTime() < reminderDate.getTime() + 60_000)
+				bot.sendMessage(chatId, message.text)
+		})
+	}, 60_000)
 }
