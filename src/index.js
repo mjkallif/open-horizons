5fb60109a9472955ff09c27ff3e2397cf64b60aa
hekotok <dan_zhel@mail.ru>
2023-12-02 02:22:52 +0200

    cringe

--- src/index.js
+++ src/index.js
@@ -17,9 +17,13 @@ import {
 } from './utils/admin.js'
 
 const start = async ({ chat }) => {
+	const fileContent = fs.readFileSync('tempdb.json', 'utf8')
+	const parsedData = fileContent ? JSON.parse(fileContent) : {}
+	const helloText = parsedData && parsedData.helloText ? parsedData.helloText : 'Привет, я буду твоим личным помощником'
+
 	await bot.sendMessage(
 		chat.id,
-		JSON.parse(fs.readFileSync('tempdb.json', 'utf8')).helloText || 'Привет, я буду твоим личным помощником',
+		helloText,
 		{ reply_markup: {
 			keyboard: [ [ { text: 'Мои мероприятия' }, { text: 'Подписаться на мероприятие' } ] ]
 		} }
